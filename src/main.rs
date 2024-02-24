20e89f61895a6154fcf212b80ed693e60c7ccae3
MosesSoftEng <moses.soft.eng@gmail.com>
2024-02-24 20:48:27 +0300

    Add root route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+// Import the rocket macro
+#[macro_use] // Enables the use of its macros
+extern crate rocket;
+
+// #[get] and #[launch] are macros in the rocket crate.
+
+// Routes
+#[get("/")] // An attribute that declares a route with the GET method and the “/” path.
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+// Handlers
+#[launch] // An attribute that marks the entry point of the Rocket application.
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
